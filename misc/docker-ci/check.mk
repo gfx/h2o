36838d30d61ff9b2213931c82ceffa2952608346
FUJI Goro <goro@fastly.com>
2020-11-27 16:25:58 +0900

    tweaks

--- misc/docker-ci/check.mk
+++ misc/docker-ci/check.mk
@@ -32,7 +32,7 @@ _check:
 _do-check:
 	cmake $(CMAKE_ARGS) -H$(SRC_DIR) -B.
 	make all
-	make check
+	PROVE_SWITCHES=--state=save make check || PROVE_SWITCHES=--state=failed make check
 
 _fuzz:
 	$(FUZZ_ASAN) CC=clang CXX=clang++ $(MAKE) -f $(CHECK_MK) _check CMAKE_ARGS=-DBUILD_FUZZER=ON
